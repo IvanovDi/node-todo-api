3ec57c190644b24d2b16e370cd385d0af6c4cd90
Dima <dima77703011983@gmail.com>
2018-07-19 19:17:07 +0300

    Added route GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
 
 
 var {mongoose} = require('./db/mongoose');
@@ -32,6 +33,25 @@ app.get('/todos', (req, res) => {
         .catch((error) => {
             res.status(400).send(error);
         })
+});
+
+app.get('/todos/:id', (req, res) => {
+
+    var id = req.params.id;
+
+    if(!ObjectID.isValid(id)) {
+        return res.status(404).send();
+    }
+
+    Todo.findById(id).then((todo) => {
+        if (!todo) {
+            res.status(404).send();
+        }
+
+        res.status(200).send({todo});
+    })
+        .catch(err => res.status(400).send());
+
 });
 
 app.listen(3000, () => {
