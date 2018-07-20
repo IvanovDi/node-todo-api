609670ef6f27bf6ae0cc7ca058217d3dc3251d48
Dima <dima77703011983@gmail.com>
2018-07-20 18:11:56 +0300

    create config file

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 
 //fetch uri for connect to mongo server
-mongoose.connect( process.env.MONGODB_URI ||'mongodb://localhost:27017/TodoApp' );
+mongoose.connect( process.env.MONGODB_URI );
 
 module.exports = {
     mongoose
